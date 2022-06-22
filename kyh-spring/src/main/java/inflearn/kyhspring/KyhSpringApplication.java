7082d8791ebbc8b9adf8569b281e7f2d78cd0ae8
csh <kweon3309@naver.com>
2022-06-22 22:33:30 +0900

    start

---
+++ kyh-spring/src/main/java/inflearn/kyhspring/KyhSpringApplication.java
@@ -0,0 +1,13 @@
+package inflearn.kyhspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyhSpringApplication.class, args);
+	}
+
+}
---
+++ kyh-spring/src/test/java/inflearn/kyhspring/KyhSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.kyhspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyhSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
